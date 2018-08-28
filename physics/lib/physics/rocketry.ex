e93ef3fe7f2f84c55032039ba1da778da2026e2a
Arvin Wiyono <arvinwiyono@gmail.com>
2018-08-28 15:49:16 +1000

    Add Converter module

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -1,22 +1,19 @@
 defmodule Physics.Rocketry do
-  def escape_velocity(planet) do
-    planet
-      |> calculate_escape
-      |> convert_to_km
-      |> rounded_to_nearest_tenth
+  def escape_velocity(:earth) do
+    %{ mass: 5.972e24, radius: 6.371e6 }
+      |> escape_velocity
   end
 
-  def calculate_escape(%{ mass: mass, radius: radius }) do
+  def escape_velocity(planet) when is_map(planet) do
+    planet
+      |> calculate_escape
+      |> Physics.Converter.to_km
+      |> Physics.Converter.to_nearest_tenth
+  end
+
+  defp calculate_escape(%{ mass: mass, radius: radius }) do
     newtons_constant = 6.67e-11
     2 * newtons_constant * mass / radius
       |> :math.sqrt
-  end
-
-  def convert_to_km(val) do
-    val / 1000
-  end
-
-  def rounded_to_nearest_tenth(val) do
-    val |> Float.ceil(1)
   end
 end
