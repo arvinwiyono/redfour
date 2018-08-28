93b08604767b9839f7165b0299e6fade6fc3a33c
Arvin Wiyono <arvinwiyono@gmail.com>
2018-08-28 16:46:23 +1000

    Modify Converter module

--- physics/test/converter_test.exs
+++ physics/test/converter_test.exs
@@ -2,7 +2,7 @@ defmodule ConverterTest do
   use ExUnit.Case
 
   test "to to_nearest_tenth" do
-    assert Converter.to_nearest_tenth(10.11) == 10.1
+    assert Converter.to_nearest_tenth(10.11) == 10.2
     assert Converter.to_nearest_tenth(10.55) == 10.6
   end
 
@@ -11,6 +11,6 @@ defmodule ConverterTest do
   end
 
   test "to_light_seconds" do
-    assert Converter.to_light_seconds({:miles, 0.1e11}, precision: 1) == 53681
+    assert {:miles, 0.1e11} |> Converter.to_light_seconds == 53681
   end
 end
