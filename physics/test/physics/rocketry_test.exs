b262f33c8c50cc25091b00508acb67b17cd60bf5
Arvin Wiyono <arvinwiyono@gmail.com>
2018-09-14 11:26:02 +1000

    Add escape velocity for Mars and moon

--- physics/test/physics/rocketry_test.exs
+++ physics/test/physics/rocketry_test.exs
@@ -4,4 +4,12 @@ defmodule RocketryTest do
   test "earth escape_velocity" do
     assert Physics.Rocketry.escape_velocity(:earth) == 11.2
   end
+
+  test "mars escape_velocity" do
+    assert Physics.Rocketry.escape_velocity(:mars) == 5.0
+  end
+
+  test "moon escape_velocity" do
+    assert Physics.Rocketry.escape_velocity(:moon) == 2.4
+  end
 end
